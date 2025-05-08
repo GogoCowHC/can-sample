844f44f636a53d8ee93399e50d1679067123cbae
GoGoCow <gogocow.hc@gmail.com>
2025-05-08 21:56:17 +0700

    modified:   .gitignore 	new file:   CMakeLists.txt 	new file:   Makefile 	modified:   README.md 	new file:   hdr/generic.h 	new file:   src/main.c

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # can-sample
+
+
+> git clone https://github.com/GogoCowHC/can-sample.git
+
+> cmake can-sample
+
+> cd can-sample
+
+> make
+
+> ./can-sample
---
+++ hdr/generic.h
@@ -0,0 +1,23 @@
+#ifndef generic_h
+#define generic_h
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+
+
+
+
+/* declare function here*/
+
+
+inline void print_dummy(const char *str) {
+    printf("%s\n", str);
+}
+
+#endif
+// This file is part of the C Generic Library.
