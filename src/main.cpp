972c8d378b5a60eac75b5a61db87baa9d8d92c50
GoGoCow <gogocow.hc@gmail.com>
2025-05-10 16:35:57 +0700

    modified:   CMakeLists.txt 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,59 @@
 #include <iostream>
 
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
 #include <linux/can.h>
+#include <linux/can/raw.h>
 
-can_frame frame;
-int main() {
-    // Example usage of the generic functions
-    test();
-    std::cout << "Hello from main!" << std::endl;
-    
-    frame.can_id = 0x123;
+int main(int argc, char **argv)
+{
+	int s; 
+	sockaddr_can addr;
+	ifreq ifr;
+	can_frame frame;
 
-    std::cout <<  frame.can_id << std::endl;
-    return 0;
+	printf("CAN Sockets Demo\r\n");
+
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("Socket");
+		return 1;
+	}
+
+	strcpy(ifr.ifr_name, "vcan0" );
+	ioctl(s, SIOCGIFINDEX, &ifr);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("Bind");
+		return 1;
+	}
+
+	frame.can_id = 0x555;
+	frame.can_dlc = 5;
+	std::cout << frame.data<< "Hello" <<std::endl;
+
+	if (write(s, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
+		perror("Write");
+		return 1;
+	}
+
+	if (close(s) < 0) {
+		perror("Close");
+		return 1;
+	}
+
+    std::cout << std::endl << " This is end of code test!! \r\r " << std::endl;
+	return 0;
 }
